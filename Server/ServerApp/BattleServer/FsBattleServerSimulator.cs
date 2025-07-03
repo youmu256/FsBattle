b0210c3f56a4a62e3184b68d97d76c4363e560b4
zzj <1013578824@qq.com>
2025-07-03 15:04:34 +0800

    ServerConsole

---
+++ Server/ServerApp/BattleServer/FsBattleServerSimulator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FrameSyncBattle;
+
+namespace ConsoleApp1
+{
+    public class FsBattleServerLogger : ILogger
+    {
+        public void LogError(object msg, object param)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void LogWarning(object msg, object param)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void Log(object msg, object param)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    
+    public class FsBattleServerSimulator
+    {
+        public void TestBattle()
+        {
+            FsBattleLogic battleLogic = new FsBattleGame();
+            battleLogic.Init(15,0,new FsBattleStartData());
+            FsDebug.Set(new FsBattleServerLogger());
+            FsDebug.Log("Battle Inited");
+        }
+    }
+}
---
+++ Server/ServerApp/BattleServer/Program.cs
@@ -0,0 +1,8 @@
+// See https://aka.ms/new-console-template for more information
+
+using System;
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+new FsBattleServerSimulator().TestBattle();
